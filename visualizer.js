0586dba0d5e31bd4260ab813ac1fe833c3ad3014
Srikar Manikonda <avatsara3@gmail.com>
2024-07-22 20:40:53 -0500

    should get somewhere

--- visualizer.js
+++ visualizer.js
@@ -45,10 +45,10 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     if (document.getElementById('base-msrp-viz')) {
-        createBarChart('#base-msrp-viz', msrpHardcodedData, 'Model', 'Base MSRP', false, 'Base MSRP of Electric Vehicles');
+        createBarChart('#base-msrp-viz', msrpHardcodedData, 'Model', 'Base MSRP', 'Base MSRP of Electric Vehicles');
     }
 
-    function createBarChart(container, data, xLabel, yLabel, isHorizontal = false, title) {
+    function createBarChart(container, data, xLabel, yLabel, title) {
         const margin = { top: 50, right: 50, bottom: 50, left: 50 };
         const width = 800 - margin.left - margin.right;
         const height = 400 - margin.top - margin.bottom;
@@ -175,7 +175,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         svg.append('text')
             .attr('x', 0)
-            .attr('y', 0)
+            .attr('y', -height / 2 + 20)
             .attr('text-anchor', 'middle')
             .style('font-size', '20px')
             .text(title);
@@ -209,8 +209,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     end: "arrow"
                 },
                 color: ["#E8336D"],
-                x: arc.centroid(pie(data)[0])[0],
-                y: arc.centroid(pie(data)[0])[1],
+                x: width / 2 + radius + 10,
+                y: -height / 2 + 20,
                 dx: 50,
                 dy: -50
             },
@@ -224,8 +224,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     end: "arrow"
                 },
                 color: ["#E8336D"],
-                x: arc.centroid(pie(data)[4])[0],
-                y: arc.centroid(pie(data)[4])[1],
+                x: -width / 2 - radius - 10,
+                y: height / 2 - 20,
                 dx: -50,
                 dy: 50
             }
@@ -233,7 +233,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const makeAnnotations = d3.annotation()
             .annotations(annotations)
-            .type(d3.annotationCalloutCircle)
+            .type(d3.annotationCallout)
             .accessors({
                 x: d => d.x,
                 y: d => d.y
