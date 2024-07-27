fe9d2cb013ee80c9bf2ff2a3d551bd91d146bb82
Srikar Manikonda <avatsara3@gmail.com>
2024-07-27 11:45:11 -0700

    Revert "go back to data"
    This reverts commit 17b5f0c26cd56a704e9232109be56d8106ce5a12.

--- visualizer.js
+++ visualizer.js
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', async function() {
 
     console.log('MSRP Data:', msrpData);
 
-    const vehicleTypeCounts = data.reduce((acc, d) => {
+    const vehicleTypeCounts = filteredData.reduce((acc, d) => {
         acc[d['Electric Vehicle Type']] = (acc[d['Electric Vehicle Type']] || 0) + 1;
         return acc;
     }, {});
