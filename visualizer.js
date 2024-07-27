866e6b2a5aba51a3d29a8fc376b71a72e3e21a57
Srikar Manikonda <avatsara3@gmail.com>
2024-07-27 11:54:56 -0700

    fix

--- visualizer.js
+++ visualizer.js
@@ -59,7 +59,7 @@ document.addEventListener('DOMContentLoaded', async function() {
         value: +d['Base MSRP']
     }));
     console.log('MSRP Data:', msrpData);
-    const vehicleTypeCounts = filteredData.reduce((acc, d) => {
+    const vehicleTypeCounts = data.reduce((acc, d) => {
         acc[d['Electric Vehicle Type']] = (acc[d['Electric Vehicle Type']] || 0) + 1;
         return acc;
     }, {});
